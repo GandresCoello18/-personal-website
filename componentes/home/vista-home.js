0176768c2af0b7b3c7023c0de736d805e12ff63d
GandresCoello18 <goyeselecoca@gmail.com>
2020-04-20 15:57:44 -0500

    probando token por headers

--- componentes/home/vista-home.js
+++ componentes/home/vista-home.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const { config } = require('../../config/index');
 const ruta_api = require('../../util/util');
 const cache = require('../../util/cacheResponse');
 const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS } = require('../../util/time');
@@ -9,8 +10,12 @@ const router = express.Router();
 
 router.get('/', async function(req, res){
     cache(res, FIVE_MINUTES_IN_SECONDS);
-    const proyecto = await axios.get(`${ruta_api.dominio()}/api?opcion=recientes_proyectos`);
-    const articulos = await axios.get(`${ruta_api.dominio()}/api?opcion=recientes_articulos`);
+    const proyecto = await axios.get(`${ruta_api.dominio()}/api?opcion=recientes_proyectos`,{
+        headers: { "access-token": config.token }
+    });
+    const articulos = await axios.get(`${ruta_api.dominio()}/api?opcion=recientes_articulos`, {
+        headers:{ "access-token": config.token }
+    });
     
     const pasa = proyecto.data;
     const pasa_2 = articulos.data
