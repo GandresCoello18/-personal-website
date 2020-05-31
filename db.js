dd7696af8dfb89be56fa1befb6444cc065de6bab
GandresCoello18 <goyeselecoca@gmail.com>
2020-05-30 23:38:42 -0500

    reconnexion

--- db.js
+++ db.js
@@ -1,34 +1,34 @@
 var mysql = require("mysql");
 const { config } = require("./config/index");
 
-function connectar() {
-  var connection = mysql.createPool({
+function handleDisconnect() {
+  let configuracion = {
     host: config.host,
     port: 3306,
     user: config.user,
     password: config.password,
     database: config.database,
-  });
+  };
 
-  connection.connect((err) => {
+  const connection = mysql.createConnection(configuracion);
+
+  connection.connect(function (err) {
     if (err) {
-      console.error(new Error(err));
-      //setTimeout(connectar(), 2000);
-    } else {
-      console.log("conectado con exito");
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000);
+      connection.on("error", function (err) {
+        console.log("db error", err);
+        if (err.code === "PROTOCOL_CONNECTION_LOST") {
+          handleDisconnect();
+        } else {
+          throw err;
+        }
+      });
     }
-  });
-
-  connection.on("err", (err) => {
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      console.log("conetado db");
-      setTimeout(connectar(), 2000);
-    } else {
-      throw err;
-    }
+    handleDisconnect();
   });
 
   return connection;
 }
 
-module.exports = connectar();
+module.exports = handleDisconnect();
