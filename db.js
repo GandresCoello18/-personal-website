7aa0dc338ca0d0f27b77b550e78be0de7adcf507
GandresCoello18 <goyeselecoca@gmail.com>
2020-05-30 22:53:22 -0500

    test con Pool

--- db.js
+++ db.js
@@ -1,38 +1,34 @@
-var mysql = require('mysql');
-const { config } = require('./config/index');
+var mysql = require("mysql");
+const { config } = require("./config/index");
 
+function connectar() {
+  var connection = mysql.createPool({
+    host: config.host,
+    port: 3306,
+    user: config.user,
+    password: config.password,
+    database: config.database,
+  });
 
-  function connectar(){
-    var connection = mysql.createConnection({
-      host: config.host,
-      user: config.user,
-      password: config.password,
-      database: config.database,
-    });
-  
-    connection.connect( (err) => {
-      if(err){
-        console.error(new Error(err));
-        setTimeout(connectar(), 2000);
-      }else{
-        console.log('conectado con exito');
-      }
-    });
-  
-    connection.on('err', err => {
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-        
-        console.log('conetado db');
-        setTimeout(connectar(), 2000);              
-      
-      } else {    
-        setTimeout(connectar(), 2000);                              
-        throw err;
-      }
-    });
+  connection.connect((err) => {
+    if (err) {
+      console.error(new Error(err));
+      //setTimeout(connectar(), 2000);
+    } else {
+      console.log("conectado con exito");
+    }
+  });
 
-    return connection;
-  }
+  connection.on("err", (err) => {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.log("conetado db");
+      setTimeout(connectar(), 2000);
+    } else {
+      throw err;
+    }
+  });
 
+  return connection;
+}
 
 module.exports = connectar();
