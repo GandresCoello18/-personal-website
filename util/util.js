6ae05aa8dedc753db5cb6e9bb7c93da45d45c4cb
GandresCoello18 <goyeselecoca@gmail.com>
2020-06-05 16:48:57 -0500

    update domain production

--- util/util.js
+++ util/util.js
@@ -1,24 +1,23 @@
-var axios = require('axios');
-const { config } = require('../config/index');
+var axios = require("axios");
+const { config } = require("../config/index");
 
-function dominio(){
-    return "http://127.0.0.1:3000";
-    //return "https://andres-coello-goyes.herokuapp.com";
+function dominio() {
+  //return "http://127.0.0.1:3000";
+  return "https://andres-coello-goyes.herokuapp.com";
 }
 
-function dominio_developer(){
-    return "http://127.0.0.1:3000";
+function dominio_developer() {
+  return "http://127.0.0.1:3000";
 }
 
-async function info_articulo(articulo){
-    return await axios.get(`${dominio()}/api/blog?articulo=${articulo}`, {
-        headers: { "access-token": config.token }
-    });
+async function info_articulo(articulo) {
+  return await axios.get(`${dominio()}/api/blog?articulo=${articulo}`, {
+    headers: { "access-token": config.token },
+  });
 }
-
 
 module.exports = {
-    dominio,
-    dominio_developer,
-    info_articulo
-}
+  dominio,
+  dominio_developer,
+  info_articulo,
+};
